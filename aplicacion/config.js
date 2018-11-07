062065ecbdf1a3ffd329725354e3ace9417c471d
alejohernandez95 <alejohernandez95@hotmail.com>
2018-11-07 02:15:33 -0500

    CORRECCION PEQUEÑO ERROR
    las comas

--- aplicacion/config.js
+++ aplicacion/config.js
@@ -1,7 +1,7 @@
 module.exports={
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/datos'
+  db: process.env.MONGODB || 'mongodb://localhost:27017/datos',
   //clave que decodifica los tokens
-  SECRET_TOKEN: 'miclavetokens'
+  SECRET_TOKEN: 'miclavetokens',
 }
 //////////////////////HECHO POR ALEJANDRO HERNANDEZ CASTAÑEDA
