ec956678fde8e24ee98051562dd4f34542d07586
alejohernandez95 <alejohernandez95@hotmail.com>
2018-11-06 23:55:41 -0500

    pequeños recordatorios

--- aplicacion/models/sensor.js
+++ aplicacion/models/sensor.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema
 ///////////////////////////////////////////////////////////////////
 const SensorSchema = Schema ({
   ubicacion: {type: Number, default: 0},// se define de 1 a 25 segun la zona de la ciudad
-  ////////los datos hora dia mes y año sera remplazado por //date: {type: Date, default: Date.now};
+  ////////los datos hora dia mes y año sera remplazado por //date: {type: Date, default: Date.now};//date: {type: Date, default: 8/24/2009}; {type: Date, default: Date.now()}
   hora: {type: Number, min: 0, max:24},// de momento se usa hora militar del 0 al 24
   dia: {type: Number, default:1},
   mes: {type: Number, min:1, max:12},//del 1 al 12
